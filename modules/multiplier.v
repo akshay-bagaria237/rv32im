f285e128e186552f2c7ab5e74b1c239706be31c7
Arsh <aarshverma.2006@gmail.com>
2026-04-09 15:40:00 +0530

    Complete hardware multiplier signed bounds and truncation logic

--- modules/multiplier.v
+++ modules/multiplier.v
@@ -10,4 +10,12 @@ module multiplier (
 `include "opcode.vh"
 
 wire [63:0] m_s_s = $signed(operand1) * $signed(operand2);
+wire [63:0] m_u_u = $unsigned(operand1) * $unsigned(operand2);
+wire [63:0] m_s_u = $signed(operand1) * $signed({1'b0, operand2});
+
+assign result = (funct3 == MUL)    ? m_s_s[31:0] :
+                (funct3 == MULH)   ? m_s_s[63:32] :
+                (funct3 == MULHSU) ? m_s_u[63:32] :
+                (funct3 == MULHU)  ? m_u_u[63:32] : 32'h0;
+
 endmodule
