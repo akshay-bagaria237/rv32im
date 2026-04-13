bf4e26c29818dd7e51886fc6ca129716b7b271d4
Abhiraj <sabhiraj2023@gmail.com>
2026-04-13 15:20:00 +0530

    Implement combinatorial hit detection and multiplexers

--- modules/l1_cache.v
+++ modules/l1_cache.v
@@ -26,4 +26,17 @@ module l1_cache #(
 
     reg [1:0] replace_ptr [0:SETS-1];
 
+    // --- COMBINATORIAL HIT DETECTION ---
+    wire hit0 = valid_array[index][0] && (tag_array[index][0] == tag);
+    wire hit1 = valid_array[index][1] && (tag_array[index][1] == tag);
+    wire hit2 = valid_array[index][2] && (tag_array[index][2] == tag);
+    wire hit3 = valid_array[index][3] && (tag_array[index][3] == tag);
+    
+    assign hit = (re || we) ? (hit0 || hit1 || hit2 || hit3) : 1'b0;
+    
+    assign rdata = hit0 ? data_array[index][0] :
+                   hit1 ? data_array[index][1] :
+                   hit2 ? data_array[index][2] :
+                   hit3 ? data_array[index][3] : 32'h0;
+
 endmodule
