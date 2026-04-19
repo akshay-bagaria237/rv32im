914653326fbd2e4b9393b02ea24c6200679b3a94
Aryan <mraryanchakravorty@gmail.com>
2026-04-19 14:10:00 +0530

    Finalize 7-segment metric indicators linking for predictor

--- demos/bpb_demo/top_bpb_fpga.v
+++ demos/bpb_demo/top_bpb_fpga.v
@@ -102,4 +102,25 @@ module top_bpb_fpga #(
     // Accuracy: (hit * 100) / total
     wire [15:0] accuracy = (total_count == 0) ? 0 : (hit_count * 100) / total_count;
     assign led = accuracy;
+
+    // 7-segment display logic
+    reg [3:0] hex_digit;
+    always @(*) begin
+        an = 8'b11111111;
+        case (clk_div[18:17])
+            2'b00: begin an[0] = 0; hex_digit = accuracy % 10; end
+            2'b01: begin an[1] = 0; hex_digit = (accuracy / 10) % 10; end
+            2'b10: begin an[2] = 0; hex_digit = (accuracy / 100) % 10; end
+            2'b11: begin an[3] = 0; hex_digit = 4'hF; end // Empty
+        endcase
+    end
+
+    always @(*) begin
+        case (hex_digit)
+            4'h0: seg = 7'b1000000; 4'h1: seg = 7'b1111001; 4'h2: seg = 7'b0100100; 4'h3: seg = 7'b0110000;
+            4'h4: seg = 7'b0011001; 4'h5: seg = 7'b0010010; 4'h6: seg = 7'b0000010; 4'h7: seg = 7'b1111000;
+            4'h8: seg = 7'b0000000; 4'h9: seg = 7'b0010000; 
+            default: seg = 7'b1111111; 
+        endcase
+    end
 endmodule
