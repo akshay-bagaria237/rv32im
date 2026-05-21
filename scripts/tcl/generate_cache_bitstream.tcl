bbbb16b275205332051ed121e76a7ad7b7bf8958
Aryan <mraryanchakravorty@gmail.com>
2026-05-21 20:47:32 +0530

    Optimize script: generate_cache_bitstream.tcl

---
+++ scripts/tcl/generate_cache_bitstream.tcl
@@ -0,0 +1,16 @@
+open_project riscv-32im.xpr
+
+reset_run synth_1
+reset_run impl_1
+
+launch_runs synth_1 -jobs 8
+wait_on_run synth_1
+
+launch_runs impl_1 -to_step write_bitstream -jobs 8
+wait_on_run impl_1
+
+open_run impl_1
+set bit_path [get_property BITSTREAM.FILE [current_design]]
+file copy -force $bit_path "./demos/cache_mixed_50/cache_mixed_50.bit"
+
+close_project
